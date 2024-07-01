f2922b9f4db163cd17a056138ca7d905c5a2165f
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-07-01 23:23:58 +0530

    Updated

---
+++ 010. For Loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Loop from 0 to 5 inclusive
+    for (int i = 0; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl; // Output the current value of i
+    }
+    return 0;
+}
