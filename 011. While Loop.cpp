c48bc6c968527c2b3f32a02825ada7db4c7dd9e3
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2024-07-02 20:17:13 +0530

    Updated

---
+++ 011. While Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int i = 0; // Initialize variable i to 0
+
+    // Execute the loop while i is less than 5
+    while (i <= 5) {
+        std::cout << "i = " << i << std::endl; // Output the current value of i
+        i++; // Increment i by 1
+    }
+
+    return 0;
+}
