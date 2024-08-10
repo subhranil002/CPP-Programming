a368ce7765c5a551154a54614e265f185e001840
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2024-08-10 17:36:04 +0000

    Updated

---
+++ 030. Typedef.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Create a type alias for an integer
+typedef int Integer;
+
+int main() {
+    Integer a = 10;
+    std::cout << "a = " << a << std::endl;
+
+    return 0;
+}
