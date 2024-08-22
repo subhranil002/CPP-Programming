3d317d78aad87054ac96205e7d39b67fde909ae8
Subhranil Chakraborty <106914208+subhranil002@users.noreply.github.com>
2024-08-22 18:02:02 +0000

    Updated

---
+++ 031. Using.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Create a type alias for an integer using 'using'
+using Integer = int;
+
+int main() {
+    Integer a = 20;
+    std::cout << "a = " << a << std::endl;
+
+    return 0;
+}
